c295aeeeb9a1053921e20d8afe37f7bb21aa7164
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-04-22 15:39:03 +0000

    mv

--- MERN/Networking_fundamentals/PCC-CS692-CN-main/PCC-CS692-CN-main/Assignment1/a/bin.c
+++ MERN/Networking_fundamentals/PCC-CS692-CN-main/PCC-CS692-CN-main/Assignment1/a/bin.c
@@ -2,18 +2,18 @@
  * @file sender.c
  * @brief Implementation of a message sending system using System V message queues.
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <stdio.h> // Include standard input/output library for functions like printf and fgets
+#include <stdlib.h> // Include standard library for general purpose functions
+#include <string.h> // Include string library for string manipulation functions like strlen
+#include <sys/types.h> // Include system types for data types like key_t
+#include <sys/ipc.h> // Include System V IPC library for inter-process communication
+#include <sys/msg.h> // Include System V message queue library for message queue functions
 
-#define MESSAGE_KEY 2832
-#define PERMISSIONS 0666
+#define MESSAGE_KEY 2832  // Unique key for the message queue
+#define PERMISSIONS 0666  // Read and write permissions for the owner, group, and others
 
 struct message_struct {
-  long int message_type;
+  long int message_type; 
   char message_body[BUFSIZ];
 };
 
@@ -25,7 +25,7 @@ int main() {
   }
 
   struct message_struct message;
-  message.message_type = 1;
+  message.message_type = 1; // Set message type to 1 for sending messages
 
   while(1) {
     printf("Enter message (type 'end' to quit): ");
