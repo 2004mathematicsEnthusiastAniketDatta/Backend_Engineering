10cb9c13d17d02332d82ac835724cdb4eaba88bc
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-04-22 16:37:14 +0000

    bin

--- MERN/Networking_fundamentals/PCC-CS692-CN-main/PCC-CS692-CN-main/Assignment1/a/bin.c
+++ MERN/Networking_fundamentals/PCC-CS692-CN-main/PCC-CS692-CN-main/Assignment1/a/bin.c
@@ -16,15 +16,24 @@ struct message_struct {
   long int message_type; 
   char message_body[BUFSIZ];
 };
-
+// key_t ftok (const char *pathname, int proj_id);
+// This function generates a unique key for the message queue using the specified pathname and project identifier.
+// The pathname is typically a file that exists in the filesystem, and proj_id is an integer that helps to create a unique key.
+// The generated key can be used with msgget() to create or access a message queue.
 int main() {
-  int msgid = msgget((key_t)MESSAGE_KEY, PERMISSIONS | IPC_CREAT);
+  // Creating or Opening a message Queue
+  int msgid = msgget((key_t)MESSAGE_KEY, PERMISSIONS | IPC_CREAT); //the second arguement msgflag is normally IPC_CREAT |0666
+  // on success , msgget(...) returns a message queue identifier
+  // on failure, -1 is returned
+  //IPC_PRIVATE constant can be placed as first arguement/
   if (msgid == -1) {
     perror("msgget failed");
     exit(EXIT_FAILURE);
   }
-
-  struct message_struct message;
+  // A parent process creates message queue prior to performing a fork() , and the child inherits the returned message queue identifier.
+  //For unrelated processes we can use this constant , the creator process has to write the returned 
+  //message queue identifier in a file that can be read by the other process.
+  struct message_struct message; // Define a message structure to hold the message type and body
   message.message_type = 1; // Set message type to 1 for sending messages
 
   while(1) {
@@ -37,7 +46,31 @@ int main() {
       message.message_body[len-1] = '\0';
     }
 
-    // Send the message
+    // Send the message 
+    // The msgsnd system call  is used to send a message tp the message queue identified by the first arguement, 
+    //which is the message queue identifier returned by msgget.
+    //The second identifier is a pointer to the message structure that contains the message to be sent.
+    //The third arguement is the size of the message to be sent, and the last arguement is a flag that can be used to specify
+    //whether the message should be sent immediately or if it should block until space is available in the queue.
+    // If the message queue is full, the process will block until space becomes available.
+    // If the message is sent successfully, msgsnd returns 0. If it fails, it returns -1.
+    //The fourth arguement msgflag  can be 0 or IPC_NOWAIT.
+    // If it is 0, the message is sent immediately. If it is IPC_NOWAIT, the function returns -1 if the queue is full.
+    // the msgflag is a flag that can be used to specify whether the message should be sent immediately 
+    //or if it should block until space is available in the queue.
+    // int msgrcv (int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg);
+    // The msgrcv system call is used to receive a message from the message queue identified by the first arguement,
+    // which is the message queue identifier returned by msgget.
+    // The second arguement is a pointer to the message structure that will receive the message.
+    // The third arguement is the size of the message to be received, and the fourth arguement is the message type to receive.
+    // The fifth arguement is a flag that can be used to specify whether the message should be received immediately or if it should block until a message of the specified type is available.
+    // If the message is received successfully, msgrcv returns the size of the message received. If it fails, it returns -1.
+    // The message type can be a specific value or 0 to receive any message.
+        //msgtype == 0 means that the first message from the queue is removed and returned.
+    //msgtype > 0 means that the first message from the queue with the mtype field equals to msgtype is removed 
+    //and returned to the calling process.
+    //msgtype < 0 means that the first message from the queue with the mtype field less than or equal to absolute value of msgtype 
+    //is removed & returned .
     if (msgsnd(msgid, (void *)&message, BUFSIZ, 0) == -1) {
       perror("msgsnd failed");
       exit(EXIT_FAILURE);
@@ -49,4 +82,5 @@ int main() {
   }
 
   return 0;
+
 }
