5f7fc59c8dd199a38923163f43ca61e32e2502dd
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-06-14 16:27:42 +0000

    ipv4

--- MERN/Networking_fundamentals/PCC-CS692-CN-main/PCC-CS692-CN-main/Assignment2/ipv4/ipv4_server.c
+++ MERN/Networking_fundamentals/PCC-CS692-CN-main/PCC-CS692-CN-main/Assignment2/ipv4/ipv4_server.c
@@ -24,7 +24,7 @@ int main() {
     }
 
     // Set socket options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, 
                    &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
