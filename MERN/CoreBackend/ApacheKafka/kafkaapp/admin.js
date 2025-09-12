b40a1c3874601ef39c7fb28fa1da29bb89e006a3
AniketDatta <2004aniketdatta@gmail.com>
2025-09-12 11:07:36 +0530

    kafka

--- MERN/CoreBackend/ApacheKafka/kafkaapp/admin.js
+++ MERN/CoreBackend/ApacheKafka/kafkaapp/admin.js
@@ -5,8 +5,23 @@ const kafka = new Kafka({
   brokers: ['http://192.168.1.8:9092'] // Replace with your Kafka broker addresses
 });
 async function init(){
-    const admin = kafka.admin();
-    console.log("Admin connecting...");
-    await admin.connect();
-    console.log("Admin connected");
+  const admin = kafka.admin();
+  console.log("Admin connecting...");
+  await admin.connect();
+  console.log("Admin connected");
+  
+  console.log("Creating topics...");
+  await admin.createTopics({
+    topics: [{
+      topic: 'rider-updates',
+      numPartitions: 2,
+    }]
+  });
+  console.log("Topics created successfully");
+  
+  console.log("Disconnecting admin...");
+  await admin.disconnect();
+  console.log("Admin disconnected");
 }
+
+init().catch(console.error);
