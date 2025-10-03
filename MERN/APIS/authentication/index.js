c8e264b724cda42440e66ebe1b1288d5301ed4ba
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-10-03 18:16:56 +0000

    basic structure

---
+++ MERN/APIS/authentication/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+// import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+// import authRoutes from './routes/auth.js';
+// import userRoutes from './routes/users.js';
+// import cookieParser from 'cookie-parser';
+import cors from 'cors';
+// Load environment variables from .env file
+dotenv.config();
+// Port from environment variables or default to 5000
+const app = express();
+//Saying no to SameOrigin Policy 
+// DeepSearch about the code
+//Mongoose connection to mongoDB
+import connectDB from './utils/db.js';
+app.use(cors(
+    {
+    origin:'https://friendly-space-invention-76gj6g6779v2wx9r-5000.app.github.dev/',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type','Authorization'],
+    credentials:true,
+    optionsSuccessStatus:200,
+    preflightContinue:false
+} 
+));
+// accept json data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to accept urlencoded data
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+app.get("/api/users", (req, res) => {
+  res.json({ users: ["user1", "user2", "user3"] });
+});
+app.get("/api/products", (req, res) => {
+  res.json({ products: ["product1", "product2", "product3"] });
+});
+
+// connect to db
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ MERN/APIS/authentication/utils/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {});
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1); // Exit process with failure
+    }
+}
+export default connectDB;
+//export default exports single file
+// see 
+// database creation 
+// ip whitelisting in mongoDB in testing and server's ip in production
+// username: admin
+// password: Admin@123
