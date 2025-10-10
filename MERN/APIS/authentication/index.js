f1314fafb4043f8ca28b2dc60db8efe761df8eaa
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-10-10 09:30:48 +0000

    dotenv

--- MERN/APIS/authentication/index.js
+++ MERN/APIS/authentication/index.js
@@ -30,19 +30,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to accept urlencoded data
 
 const PORT = process.env.PORT || 5000;
-// app.get("/", (req, res) => {
-//   res.send("Hello, World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
-// app.get("/api", (req, res) => {
-//   res.json({ message: "API is working!" });
-// });
-// app.get("/api/users", (req, res) => {
-//   res.json({ users: ["user1", "user2", "user3"] });
-// });
-// app.get("/api/products", (req, res) => {
-//   res.json({ products: ["product1", "product2", "product3"] });
-// });
+app.get("/api", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+app.get("/api/users", (req, res) => {
+  res.json({ users: ["user1", "user2", "user3"] });
+});
+app.get("/api/products", (req, res) => {
+  res.json({ products: ["product1", "product2", "product3"] });
+});
 //db
 // connect to db
 connectDB();
