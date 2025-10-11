09fea2f9cd21a77efcb767858ac024c774bb3460
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-10-11 18:36:33 +0000

    cookies

--- MERN/APIS/authentication/controller/user.controller.js
+++ MERN/APIS/authentication/controller/user.controller.js
@@ -16,6 +16,7 @@ const registerUser = async (req, res) => {
     });
   }
   // Check if user already exists
+  //existingUser -> variable ,User (documents) mongoose model ()  or Row in SQL database
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -24,6 +25,7 @@ const registerUser = async (req, res) => {
       });
     }
 // Create a user in database
+//database is assumed to be in another continent
     const user = await User.create({
       name,
       email,
@@ -90,6 +92,7 @@ const verifyUser = async (req, res) => {
   // find user based on token
   const user = await User.findOne({ verificationToken: token });
   //if not
+  //if bad request then invalid token
   if (!user) {
     return res.status(400).json({
       message: "Invalid token",
@@ -104,34 +107,42 @@ const verifyUser = async (req, res) => {
 };
 
 const login = async (req, res) => {
+// destructure email and password from the req.body
   const { email, password } = req.body;
-
+// validate the email or password , if not there, send bad request
   if (!email || !password) {
     return res.status(400).json({
       message: "All fields are required",
     });
   }
-
+// check if user exists
   try {
     const user = await User.findOne({ email });
+// if not send bad request
     if (!user) {
       return res.status(400).json({
         message: "Invalid email or password",
       });
     }
-
+// compares the plain text password in the request body with the hashed password stored in the database
+    // bcrypt.compare() is an asynchronous function that returns a promise, so we use await to wait for the result.
+    // isMatch will be true if the passwords match, otherwise false. 
     const isMatch = await bcrypt.compare(password, user.password);
 
     console.log(isMatch);
-
+// if not matching send bad request
     if (!isMatch) {
       return res.status(400).json({
         message: "Invalid email or password",
       });
     }
 
-    //
+    // JWT: an open, industry standard RFC 7519 method for representing claims securely between two parties.
+    //JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting 
+    // information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.
+    //JWTs can be signed using a secret (with the HMAC algorithm) or a public/private key pair using RSA or ECDSA.
 
+// Although JWTs can be encrypted to also provide secrecy between parties, we will focus on signed tokens. Signed tokens can verify the integrity of the claims contained within it, while encrypted tokens hide those claims from other parties. When tokens are signed using public/private key pairs, the signature also certifies that only the party holding the private key is the one that signed it.
     const token = jwt.sign(
       { id: user._id, role: user.role },
 
@@ -140,13 +151,20 @@ const login = async (req, res) => {
         expiresIn: "24h",
       }
     );
+    // A cookie (also known as a web cookie or browser cookie) is a small piece of data a server sends to a user's web browser. 
+    // The browser may store cookies, create new cookies, modify existing ones, and send them back to the same server with later requests. 
+    // Cookies enable web applications to store limited amounts of data and remember state information; 
+    // by default the HTTP protocol is 
+    // stateless.
+    //cookieOptions contain httpOnly, secure, maxAge properties
     const cookieOptions = {
       httpOnly: true,
       secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     };
+    // set cookie in the response
     res.cookie("token", token, cookieOptions);
-
+    // send success response to user
     res.status(200).json({
       success: true,
       message: "Login successful",
--- MERN/APIS/authentication/model/User.model.js
+++ MERN/APIS/authentication/model/User.model.js
@@ -29,9 +29,16 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
+//hooks 
+//pre-save hook to hash password before saving
+// arrow function does not bind 'this', so we use function keyword with next keyword and next() is called to proceed to the next middleware or save operation
+//I forcefully bring the hook with some business logic next() gives signal that my business logic work is done
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
+    //Hash Function: A deterministic algorithm 
+    //that maps arbitrary-size input data to a fixed-size output ,  hash/digest through mathematical transformations.
+    // Encryption,Decryption : reversible process , Hashing is not reversible and hash value can be same for same input and cannot be
+    // converted back to original input
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();
