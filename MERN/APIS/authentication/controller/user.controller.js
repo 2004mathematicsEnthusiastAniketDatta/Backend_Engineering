eb552d3193ca5c0331f103985489e2e88ccf2dca
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-10-12 15:33:01 +0000

    user_controller

--- MERN/APIS/authentication/controller/user.controller.js
+++ MERN/APIS/authentication/controller/user.controller.js
@@ -1,22 +1,25 @@
 import User from "../model/User.model.js";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
-
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 const registerUser = async (req, res) => {
-  //get data from req.body
-  // Destructure the data from req.body
+  // get data
+  //validate
+  // check if user already exists
+  // create a user in database
+  //create a verification token
+  // save token in database
+  // send token as email to user
+  // send success status to user
+
   const { name, email, password } = req.body;
-  // Validate the data
   if (!name || !email || !password) {
-   // empty fields
     return res.status(400).json({
       message: "All fields are required",
     });
   }
-  // Check if user already exists
-  //existingUser -> variable ,User (documents) mongoose model ()  or Row in SQL database
+
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -24,28 +27,27 @@ const registerUser = async (req, res) => {
         message: "User already exists",
       });
     }
-// Create a user in database
-//database is assumed to be in another continent
+
     const user = await User.create({
       name,
       email,
       password,
     });
     console.log(user);
-//if user not registered , create a new user 
+
     if (!user) {
       return res.status(400).json({
         message: "User not registered",
       });
     }
-//create a verification token
+
     const token = crypto.randomBytes(32).toString("hex");
     console.log(token);
     user.verificationToken = token;
-  // save token in database
+
     await user.save();
-  // send token as email to user
-  //send email
+
+    //send email
     const transporter = nodemailer.createTransport({
       host: process.env.MAILTRAP_HOST,
       port: process.env.MAILTRAP_PORT,
@@ -55,7 +57,7 @@ const registerUser = async (req, res) => {
         pass: process.env.MAILTRAP_PASSWORD,
       },
     });
-// what to send email from 
+
     const mailOption = {
       from: process.env.MAILTRAP_SENDEREMAIL,
       to: user.email,
@@ -64,8 +66,9 @@ const registerUser = async (req, res) => {
       ${process.env.BASE_URL}/api/v1/users/verify/${token}
       `,
     };
+
     await transporter.sendMail(mailOption);
-  // send success status to user
+
     res.status(201).json({
       message: "User registered successfully",
       success: true,
@@ -81,90 +84,92 @@ const registerUser = async (req, res) => {
 
 const verifyUser = async (req, res) => {
   //get token from url
+  //validate
+  // find user based on token
+  //if not
+  // set isVerified field to true
+  // remove verification token
+  // save
+  //return response
+
   const { token } = req.params;
   console.log(token);
-   //validate
   if (!token) {
     return res.status(400).json({
       message: "Invalid token",
     });
   }
-  // find user based on token
-  const user = await User.findOne({ verificationToken: token });
-  //if not
-  //if bad request then invalid token
-  if (!user) {
-    return res.status(400).json({
-      message: "Invalid token",
+  try {
+    console.log("verification started");
+
+    const user = await User.findOne({ verificationToken: token });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid token",
+      });
+    }
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    await user.save();
+
+    res.status(200).json({
+      message: "User verified successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "User not verified",
+      error,
+      success: false,
     });
   }
-  // set isVerified field to true
-  user.isVerified = true;
-  // remove verification token
-  user.verificationToken = undefined;
-  // save the user
-  await user.save();
 };
 
 const login = async (req, res) => {
-// destructure email and password from the req.body
   const { email, password } = req.body;
-// validate the email or password , if not there, send bad request
+
   if (!email || !password) {
     return res.status(400).json({
       message: "All fields are required",
     });
   }
-// check if user exists
+
   try {
     const user = await User.findOne({ email });
-// if not send bad request
     if (!user) {
       return res.status(400).json({
         message: "Invalid email or password",
       });
     }
-// compares the plain text password in the request body with the hashed password stored in the database
-    // bcrypt.compare() is an asynchronous function that returns a promise, so we use await to wait for the result.
-    // isMatch will be true if the passwords match, otherwise false. 
+
     const isMatch = await bcrypt.compare(password, user.password);
 
     console.log(isMatch);
-// if not matching send bad request
+
     if (!isMatch) {
       return res.status(400).json({
         message: "Invalid email or password",
       });
     }
 
-    // JWT: an open, industry standard RFC 7519 method for representing claims securely between two parties.
-    //JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting 
-    // information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.
-    //JWTs can be signed using a secret (with the HMAC algorithm) or a public/private key pair using RSA or ECDSA.
+    //
 
-// Although JWTs can be encrypted to also provide secrecy between parties, we will focus on signed tokens. Signed tokens can verify the integrity of the claims contained within it, while encrypted tokens hide those claims from other parties. When tokens are signed using public/private key pairs, the signature also certifies that only the party holding the private key is the one that signed it.
     const token = jwt.sign(
       { id: user._id, role: user.role },
 
-      "shhhhh",
+      process.env.JWT_SECRET,
       {
         expiresIn: "24h",
       }
     );
-    // A cookie (also known as a web cookie or browser cookie) is a small piece of data a server sends to a user's web browser. 
-    // The browser may store cookies, create new cookies, modify existing ones, and send them back to the same server with later requests. 
-    // Cookies enable web applications to store limited amounts of data and remember state information; 
-    // by default the HTTP protocol is 
-    // stateless.
-    //cookieOptions contain httpOnly, secure, maxAge properties
     const cookieOptions = {
       httpOnly: true,
       secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     };
-    // set cookie in the response
     res.cookie("token", token, cookieOptions);
-    // send success response to user
+
     res.status(200).json({
       success: true,
       message: "Login successful",
@@ -178,4 +183,71 @@ const login = async (req, res) => {
   } catch (error) {}
 };
 
-export { registerUser, verifyUser, login };
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    console.log(user);
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log("Error in get me", error);
+  }
+};
+const logoutUser = async (req, res) => {
+  try {
+    res.cookie("token", "", {});
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {}
+};
+const forgotPassword = async (req, res) => {
+  try {
+    //get email
+    // find user based on email
+    // reset token + reset expiry => Date.now() + 10 * 60 * 1000 => user.save()
+    // send mail => design url
+  } catch (error) {}
+};
+const resetPassword = async (req, res) => {
+  try {
+    //collect token from params
+    // password from req.body
+    const { token } = req.params;
+    const { password, confPassword } = req.body;
+
+    if (password === confPassword) {
+    }
+
+    try {
+      const user = await User.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpires: { $gt: Date.now() },
+      });
+      // set password in user
+      // resetToken, resetExpiry => reset
+      // save
+    } catch (error) {}
+  } catch (error) {}
+};
+
+export {
+  registerUser,
+  verifyUser,
+  login,
+  getMe,
+  logoutUser,
+  resetPassword,
+  forgotPassword,
+};
