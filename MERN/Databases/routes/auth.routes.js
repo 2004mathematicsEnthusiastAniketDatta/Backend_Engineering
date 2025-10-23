e2a1203bc5e353b447694d4d22b56e444aaf8dcb
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-10-23 07:57:41 +0000

    changes

--- MERN/Databases/routes/auth.routes.js
+++ MERN/Databases/routes/auth.routes.js
@@ -1,12 +1,7 @@
 import express from "express";
 //import all controllers
 import {
-  login,
-  registerUser,
-  verifyUser,
-  getMe,
-  logoutUser,
-  forgotPassword
+  registerUser
 } from "../controller/auth.controller.js";
 import { isLoggedIn } from "../middleware/auth.middleware.js";
 // router 
@@ -15,12 +10,12 @@ const userRouter = express.Router();
 //routes flow and setup 
 //route -> controller
 userRouter.post("/register", registerUser);
-userRouter.get("/verify/:token", verifyUser);
-userRouter.post("/login", login);
-//route -> middleware -> controller
-userRouter.get("/profile", isLoggedIn, getMe);
-userRouter.get("/logout", isLoggedIn, logoutUser);
-userRouter.post("/forgotpassword", forgotPassword);
-userRouter.post("/resetpassword/:token", forgotPassword);
+// userRouter.get("/verify/:token", verifyUser);
+// userRouter.post("/login", login);
+// //route -> middleware -> controller
+// userRouter.get("/profile", isLoggedIn, getMe);
+// userRouter.get("/logout", isLoggedIn, logoutUser);
+// userRouter.post("/forgotpassword", forgotPassword);
+// userRouter.post("/resetpassword/:token", forgotPassword);
 //export router
 export default userRouter;
