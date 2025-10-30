4d980aaa62522f4f6a9779b3d1b1d11cb376822c
2004_Aniket_Datta <2004aniketdatta@gmail.com>
2025-10-30 11:05:56 +0530

    commit

--- Project/src/models/User.models.js
+++ Project/src/models/User.models.js
@@ -1,56 +1,64 @@
 import mongoose , {Schema} from "mongoose";
-
+import {} from "bcrypt";
 const userSchema = new Schema({
-     avatar: {
-           type: {
-                 url: String,
-                 localpath: String
-           },
-           default:{
-            url:`https://placehold.co/600x400`,
-            localpath:""
-           }
-     },
-     username:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase:true,
-        trim:true,
-        index:true
-     },
-     email:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase:true,
-        trim:true,
-        index:true
-     },
-     fullname:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase:true,
-        trim:true,
-     },
-     password:{
-        type: String,
-        required: [true,"Password is required"],
-     },
-     isEmailVerified:{
-        type: Boolean,
-        default: false,
-     },
-     forgotPasswordToken:{
-         type: Boolean,
-         default: false,
-     },
-     forgotPasswordExpiry:{
+   avatar: {
+       type: {
+           url: String,
+           localpath: String
+       },
+       default:{
+        url:`https://placehold.co/600x400`,
+        localpath:""
+       }
+   },
+   username:{
+      type: String,
+      required: true,
+      unique: true,
+      lowercase:true,
+      trim:true,
+      index:true
+   },
+   email:{
+      type: String,
+      required: true,
+      unique: true,
+      lowercase:true,
+      trim:true,
+      index:true
+   },
+   fullname:{
+      type: String,
+      required: true,
+      unique: true,
+      lowercase:true,
+      trim:true,
+   },
+   password:{
+      type: String,
+      required: [true,"Password is required"],
+   },
+   isEmailVerified:{
+      type: Boolean,
+      default: false,
+   },
+   forgotPasswordToken:{
        type: String,
-       default:  
-     }         
-},{timestamps:true});
+   },
+   forgotPasswordExpiry:{
+     type: Date,
+   },
+   refreshToken:{
+      type: String,
+   },
+   emailVerificationToken:{
+       type: String,
+   },
+   emailVerificationExpiry:{
+      type: Date,
+   }
 
+},{timestamps:true});
+userSchema.pre("save",async function(next))
 
 export const User = mongoose.model("User",userSchema);
